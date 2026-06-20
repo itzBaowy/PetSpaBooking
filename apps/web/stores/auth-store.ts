f478281b950f66192500362b64f74f117f07023b
@fsa_ThienPC <phantoanbd@gmail.com>
2026-06-20 10:33:10 +0700

    Giải quyết xung đột nhẹ vcl

--- apps/web/stores/auth-store.ts
+++ apps/web/stores/auth-store.ts
@@ -1,42 +1,34 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-import type { User } from "@/types/auth";
 import { createCookieStorage } from "./cookie-store";
 import { User } from "@/types/auth";
 
 interface Auth {
-  user: User | null;
   accessToken: string | null;
-  isAuthenticated: boolean;
-  setTokens: ({ accessToken }: { accessToken: string }) => void;
-  setUser: (user: User | null) => void;
+  refreshToken: string | null;
+  setTokens: ({
+    accessToken,
+    refreshToken,
+  }: {
+    accessToken: string;
+    refreshToken: string;
+  }) => void;
   clearTokens: () => void;
-  logout: () => void;
 }
 
 export const useAuthStore = create<Auth>()(
   persist(
     (set) => ({
-      user: null,
       accessToken: null,
-      isAuthenticated: false,
-      setTokens: ({ accessToken }) => {
-        set({ accessToken, isAuthenticated: true });
-      },
-      setUser: (user) => set({ user, isAuthenticated: Boolean(user) }),
-      clearTokens: () => set({ accessToken: null, isAuthenticated: false }),
-      logout: () =>
-        set({ user: null, accessToken: null, isAuthenticated: false }),
+      refreshToken: null,
+      setTokens: ({ accessToken, refreshToken }) =>
+        set({ accessToken, refreshToken }),
+      clearTokens: () => set({ accessToken: null, refreshToken: null }),
     }),
     {
       name: "auth-storage",
       skipHydration: true,
       storage: createJSONStorage(() => createCookieStorage()),
-      partialize: (state) => ({
-        user: state.user,
-        accessToken: state.accessToken,
-        isAuthenticated: state.isAuthenticated,
-      }),
     },
   ),
 );
