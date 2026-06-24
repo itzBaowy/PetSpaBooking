85cd7cdcc49d05cdb14e814b8fd83e97daee9439
itBaowy <notbao.js@gmail.com>
2026-06-24 17:14:47 +0700

    feat(api): provider management with approve,reject action

--- apps/api/src/routers/root.router.ts
+++ apps/api/src/routers/root.router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import authRouter from "./auth.router.ts";
 import userRouter from "./user.router.ts";
+import providerRouter from "./provider.router.ts";
 
 const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/users", userRouter);
+rootRouter.use("/providers", providerRouter);
 
 export default rootRouter;
